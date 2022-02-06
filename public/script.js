2359d23600432aac5eee4b3c006058de5fbd56db
chandu1308 <98881848+chandu1308@users.noreply.github.com>
2022-02-06 22:53:02 +0530

    update 06

---
+++ public/script.js
@@ -0,0 +1,18 @@
+// DOM elements 
+const mainPage = document.querySelector('.main-page');
+const loginPage = document.querySelector('.login-page');
+
+const middleContent = document.querySelector('.middle-content')
+
+// ***************************************************
+// ***************************************************
+
+// Main page
+const goToLoginPage = () => {
+    mainPage.style.display = 'none';
+    loginPage.style.display = 'grid';
+}
+
+middleContent.addEventListener('click', e => {
+    console.log(e.target.classList);
+})
