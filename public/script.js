ac5bebf20cd4f7aa6ef7593e8e34a062a4fab83e
chandu1308 <98881848+chandu1308@users.noreply.github.com>
2022-02-10 00:16:02 +0530

    dot

--- public/script.js
+++ public/script.js
@@ -12,6 +12,26 @@ const loginModal = document.querySelector('.login-modal');
 const modalX = document.querySelector('.login-modal i');
 
 const loginFormBtn = document.querySelector('.login-form-btn');
+
+const postBtn = document.querySelector('.post-btn');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modal = document.querySelector('.modal');
+const postModalx = document.querySelector('.modal-header i');
+
+const postHeaderBtn = document.querySelector('.modal-header button');
+const footerXbtn = document.querySelector('.modal-icons span');
+const modalInput = document.querySelector('.modal-input');
+
+const user = document.querySelector('.user');
+
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+const sidebar = document.querySelector('.sidebar');
+
+const sidebarXbtn = document.querySelector('.sidebar-header i');
+
+const toggle = document.querySelector('.toggle');
+const circle = document.querySelector('.circle');
+
 // ***************************************************
 // ***************************************************
 
@@ -23,7 +43,7 @@ const goToLoginPage = () => {
 
 
 middleContent.addEventListener('click', e => {
-    if(e.target.classList[1] === 'main-btn'){
+    if(e.target.classList.contains('main-btn')) {
 
         goToLoginPage();
 
@@ -45,7 +65,7 @@ btnTop.addEventListener('click', ()=> {
 
 // login page 
 modalX.addEventListener('click', () => {
-    loginModal.style.display = 'none'; 
+    loginModal.classList.remove('show'); 
 })
 
 loginFormBtn.addEventListener('click', () => {
@@ -55,7 +75,79 @@ loginFormBtn.addEventListener('click', () => {
         loginPage.style.display = 'none';
         newsFeedPage.style.display = 'grid';
     }else{
-        loginModal.style.display = "block";
+       loginModal.classList.add("show");
     }
 })
 
+// news feed 
+// post method 
+
+postBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+    modalWrapper.classList.add('modal-wrapper-show');
+})
+
+postModalx.addEventListener('click', () => {
+    modal.style.display = 'none';
+    modalWrapper.classList.remove('modal-wrapper-show');
+
+    if(modalInput.value !== ""){
+        modalInput.value = "";
+        changeOpacity(0.5);
+    }
+});
+
+const changeOpacity = (x) => {
+    postHeaderBtn.style.opacity = x;
+    footerXbtn.style.opacity = x;
+};
+
+modalInput.addEventListener('keypress', (e) => {
+    if(e.target.value != ""){
+        changeOpacity(1);
+    }
+});
+modalInput.addEventListener('blur', (e) => {
+    if(e.target.value === ""){
+        changeOpacity(0.5);
+    }
+});
+
+user.addEventListener('click', () => {
+    sidebarWrapper.classList.add('sidebar-wrapper-show');
+    sidebar.classList.add('sidebar-show');
+
+})
+
+sidebarXbtn.addEventListener('click', () => {
+    sidebarWrapper.classList.remove('sidebar-wrapper-show');
+    sidebar.classList.remove('sidebar-show');
+})
+
+// dark mode 
+const darkElements1 = document.querySelectorAll('.dark-mode-1');
+const darkElements2 = document.querySelectorAll('.dark-mode-2');
+const lightTexts = document.querySelectorAll('.light-text');
+const borders = document.querySelectorAll('.border-color');
+
+toggle.addEventListener('click', () => {
+    circle.classList.toggle('move');
+
+
+    Array.from(darkElements1).map((darkEl1)=> {
+        darkEl1.classList.toggle('dark-1')
+        });
+
+    Array.from(darkElements2).map((darkEl2) => {
+        darkEl2.classList.toggle('dark-2')
+        });
+    
+    Array.from(lightTexts).map((lightTxt) => {
+        lightTxt.classList.toggle('light')
+        });
+
+    Array.from(borders).map((border) => {
+        border.className.toggle('border-color')
+        });
+    
+});
